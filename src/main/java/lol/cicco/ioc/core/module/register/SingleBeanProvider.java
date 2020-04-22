1a88e8c24595614e749fd1ebd6a0df75bf73557b
Cicco <codingzx@outlook.com>
2020-04-22 23:16:46 +0800

    - remove AbstractBeanProvider

--- src/main/java/lol/cicco/ioc/core/module/register/SingleBeanProvider.java
+++ src/main/java/lol/cicco/ioc/core/module/register/SingleBeanProvider.java
@@ -1,33 +1,53 @@
 package lol.cicco.ioc.core.module.register;
 
+import javassist.util.proxy.ProxyFactory;
 import lol.cicco.ioc.annotation.Inject;
+import lol.cicco.ioc.core.module.aop.AnnotationInterceptor;
 import lol.cicco.ioc.core.module.aop.InterceptorRegistry;
+import lol.cicco.ioc.core.module.aop.JoinPointImpl;
 import lol.cicco.ioc.core.module.beans.BeanProvider;
 import lol.cicco.ioc.core.module.beans.BeanRegistry;
 import lombok.SneakyThrows;
 
 import java.lang.annotation.Annotation;
-import java.util.Arrays;
+import java.lang.reflect.Method;
+import java.util.*;
+import java.util.stream.Collectors;
 
-class SingleBeanProvider extends AbstractBeanProvider {
+class SingleBeanProvider implements BeanProvider {
 
+    private final Class<?> originCls;
+    private final InterceptorRegistry interceptorRegistry;
     private final BeanRegistry beanRegistry;
-    private final Object singleObj;
+    private final AnalyzeBeanDefine beanDefine;
+
+    private final Object targetObject;
 
     SingleBeanProvider(InterceptorRegistry interceptorRegistry, BeanRegistry beanRegistry, AnalyzeBeanDefine beanDefine) {
-        super(beanDefine.getBeanType(), interceptorRegistry);
+        this.originCls = beanDefine.getBeanType();
+        this.interceptorRegistry = interceptorRegistry;
         this.beanRegistry = beanRegistry;
+        this.beanDefine = beanDefine;
+
         // 创建实例
-        this.singleObj = createProxy(false, beanDefine.getParameterTypes(), getConstructorParams(beanDefine.getParameterTypes(), beanDefine.getParameterAnnotations()));
+        this.targetObject = createProxy();
+    }
+
+    @Override
+    public Class<?> beanType() {
+        return originCls;
     }
 
     @Override
     @SneakyThrows
     public Object getObject() {
-        return singleObj;
+        return targetObject;
     }
 
-    private Object[] getConstructorParams(Class<?>[] parameterTypes, Annotation[][] parameterAnnotations) {
+    private Object[] getConstructorParams() {
+        Class<?>[] parameterTypes = beanDefine.getParameterTypes();
+        Annotation[][] parameterAnnotations = beanDefine.getParameterAnnotations();
+
         Object[] constructorParams;
         if (parameterTypes.length == 0) { //默认构造方法
             constructorParams = new Object[]{};
@@ -61,4 +81,65 @@ class SingleBeanProvider extends AbstractBeanProvider {
         return constructorParams;
     }
 
+
+    @SneakyThrows
+    protected Object createProxy() {
+        Map<Method, Annotation[]> methodInfo = filterMethods(originCls);
+
+        ProxyFactory factory = new ProxyFactory();
+        factory.setSuperclass(originCls);
+        factory.setUseCache(false); // 不需要缓存..
+        factory.setFilter(m -> methodInfo.get(m) != null);
+
+        // 生成代理
+        return factory.create(beanDefine.getParameterTypes(), getConstructorParams(), (self, thisMethod, proceed, args) -> {
+            Annotation[] methodAnnotations = methodInfo.get(thisMethod);
+            if (methodAnnotations == null || methodAnnotations.length == 0) {
+                return proceed.invoke(self, args);
+            }
+            // 获得方法所有的拦截器
+            List<AnnotationInterceptor<?>> hasAnnotationInterceptors = Arrays.stream(methodAnnotations).map(f -> interceptorRegistry.getInterceptor(f.annotationType())).filter(Objects::nonNull).collect(Collectors.toList());
+
+            if (hasAnnotationInterceptors.isEmpty()) {
+                // 没有拦截器 直接继续执行
+                return proceed.invoke(self, args);
+            }
+            JoinPointImpl point = new JoinPointImpl(self, thisMethod, args);
+            try {
+                // 执行前置拦截器
+                for (AnnotationInterceptor<?> annotationInterceptor : hasAnnotationInterceptors) {
+                    annotationInterceptor.before(point);
+                }
+
+                Object result = proceed.invoke(self, args);
+
+                // 获得执行结果
+                point.setReturnValue(result);
+                // 执行后续拦截器
+                for (AnnotationInterceptor<?> annotationInterceptor : hasAnnotationInterceptors) {
+                    annotationInterceptor.after(point);
+                }
+                return result;
+            } catch (Exception e) {
+                point.setThrowable(e);
+                // 执行异常拦截器
+                for (AnnotationInterceptor<?> annotationInterceptor : hasAnnotationInterceptors) {
+                    annotationInterceptor.throwException(point);
+                }
+                throw e;
+            }
+        });
+    }
+
+    private Map<Method, Annotation[]> filterMethods(Class<?> superCls) {
+        Map<Method, Annotation[]> methodMap = new LinkedHashMap<>();
+        for (Method method : superCls.getDeclaredMethods()) {
+            Annotation[] annotations = method.getAnnotations();
+            if (annotations == null || annotations.length == 0) {
+                continue;
+            }
+            methodMap.put(method, annotations);
+        }
+        return methodMap;
+    }
 }
